02a5d25969d02b3f04b7dd249af1f0646d3f4cae
Taras Shyshatskyi <taras.shyshatskyi.25@kse.org.ua>
2026-06-20 18:45:21 +0300

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -1,6 +1,6 @@
 import urllib.request, pathlib
 
-URL = "https://raw.githubusercontent.com/Vadimkin/ukrainian-air-raid-sirens-dataset/main/datasets/<ТОЧНА_НАЗВА>.csv"
+URL = "https://raw.githubusercontent.com/Vadimkin/ukrainian-air-raid-sirens-dataset/main/datasets/official_data_en.csv"
 out = pathlib.Path("data/raw/alerts.csv")
 out.parent.mkdir(parents=True, exist_ok=True)
 urllib.request.urlretrieve(URL, out)
